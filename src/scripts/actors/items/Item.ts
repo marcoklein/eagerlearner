99ca028309af6833532065137144c6b6d847c4ca
Marco Klein <dxc.klein@extaccount.com>
2020-05-23 11:07:17 +0200

    run deploy only on push

--- src/scripts/actors/items/Item.ts
+++ src/scripts/actors/items/Item.ts
@@ -2,6 +2,6 @@ import { Projectile } from '../projectile/Projectile';
 import { ProjectileType } from '../projectile/ProjectileType';
 
 /**
- * I know its a little bit strange.. But a Projectile is actually anything that flys or sits around in the world and can interact with monsters/walls/ or the hero.
- * "Consumable" or EffectEntity would be a better name.n */
+ * TODO clear this up - an item is not a projectile!
+ */
 export abstract class Item extends ProjectileType {}
